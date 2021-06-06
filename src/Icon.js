039ab0d4db97c0ad3ec343390de02d5c900ea5b1
Imre Osswald <qoob@me.com>
2021-06-06 13:42:28 +0200

    adds storybook, prettier and eslint

--- src/Icon.js
+++ src/Icon.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { icons } from './shared/icons';
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { icons } from './shared/icons'
 
 const Svg = styled.svg`
-  display: ${props => (props.block ? 'block' : 'inline-block')};
-  vertical-align: middle;
+	display: ${(props) => (props.block ? 'block' : 'inline-block')};
+	vertical-align: middle;
 
-  shape-rendering: inherit;
-  transform: translate3d(0, 0, 0);
-`;
+	shape-rendering: inherit;
+	transform: translate3d(0, 0, 0);
+`
 
 const Path = styled.path`
-  fill: currentColor;
-`;
+	fill: currentColor;
+`
 
 /**
  * An Icon is a piece of visual element, but we must ensure its accessibility while using it.
@@ -23,18 +23,18 @@ const Path = styled.path`
  * - *non-decorative*: it means that it delivers information. For example, an icon as only child in a button. The meaning can be obvious visually, but it must have a proper text alternative via `aria-label` for screen readers. (ex: `<Icon icon="print" aria-label="Print this document" />`)
  */
 export function Icon({ icon, block, ...props }) {
-  return (
-    <Svg viewBox="0 0 1024 1024" width="20px" height="20px" block={block} {...props}>
-      <Path d={icons[icon]} />
-    </Svg>
-  );
+	return (
+		<Svg viewBox="0 0 1024 1024" width="20px" height="20px" block={block} {...props}>
+			<Path d={icons[icon]} />
+		</Svg>
+	)
 }
 
 Icon.propTypes = {
-  icon: PropTypes.string.isRequired,
-  block: PropTypes.bool,
-};
+	icon: PropTypes.string.isRequired,
+	block: PropTypes.bool,
+}
 
 Icon.defaultProps = {
-  block: false,
-};
+	block: false,
+}
